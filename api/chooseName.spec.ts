f44f063111e331118e7c3719c8bc0173e2d892e3
sieem <siem.linx@gmail.com>
2025-10-21 20:48:59 +0200

    feat: make sure nobody chose the same name as last year

--- api/chooseName.spec.ts
+++ api/chooseName.spec.ts
@@ -1,14 +1,32 @@
 import { dbService } from './services/db.service';
 import { chooseNamesWithRetry } from './services/chooseName.service';
+import { allNames } from './utils/allNames.utils';
 
 
 (async () => {
   await dbService.initDb();
   await dbService.clearDb();
   
-  for (let i = 2020; i < 2200; i++) {
-    console.log(`Start test: ${i}`)
-    await dbService.initYear(i);
-    await chooseNamesWithRetry(i);
+  for (let year = 2020; year < 2200; year++) {
+    console.log(`Start test: ${year}`)
+    await dbService.initYear(year);
+    await chooseNamesWithRetry(year);
+
+    const combos: Record<string, string> = {};
+    
+    for (let name of allNames) {
+      const chosenName = await dbService.getChosenName(name, year);
+
+      combos[name] = chosenName;
+      console.log(`${name} => ${chosenName}`);
+    }
+    
+    for (let name of allNames) {
+      const chosenNameLastYear = await dbService.getChosenName(name, year - 1);
+
+      if (combos[name] === chosenNameLastYear) {
+        throw `same name as last year: ${name} => ${chosenNameLastYear}`;  
+      }
+    }
   }
 })()
